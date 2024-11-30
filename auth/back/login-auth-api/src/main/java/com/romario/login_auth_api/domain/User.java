6ff22ab019c678148e0e338a5c4f147631aae744
fco-romario <fco.romario.alves@gmail.com>
2024-11-30 19:43:43 -0300

    Criando class User

---
+++ auth/back/login-auth-api/src/main/java/com/romario/login_auth_api/domain/User.java
@@ -0,0 +1,27 @@
+package com.romario.login_auth_api.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "USERS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+}

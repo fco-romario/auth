806d80f40dbaf3cc5b5c8b64287c835f252cfe4c
fco-romario <fco.romario.alves@gmail.com>
2024-11-30 23:17:14 -0300

    JWT - Criando Class AuthController e DTOs

---
+++ auth/back/login-auth-api/src/main/java/com/romario/login_auth_api/dto/RegisterRequestDTO.java
@@ -0,0 +1,3 @@
+package com.romario.login_auth_api.dto;
+
+public record RegisterRequestDTO(String name, String email, String password) {}
---
+++ auth/back/login-auth-api/src/main/java/com/romario/login_auth_api/dto/ResponseDTO.java
@@ -0,0 +1,3 @@
+package com.romario.login_auth_api.dto;
+
+public record ResponseDTO(String name, String token) {}

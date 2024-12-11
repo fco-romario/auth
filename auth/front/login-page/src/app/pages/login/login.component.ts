3d34ff027410001e219a62d7ca08602b2296c9c5
fco-romario <fco.romario.alves@gmail.com>
2024-12-10 21:57:50 -0300

    Criando componentes e melhorando css

--- auth/front/login-page/src/app/pages/login/login.component.ts
+++ auth/front/login-page/src/app/pages/login/login.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    DefaultLoginLayoutComponent
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  
+  loginForm!: FormGroup;
+
+  constructor(
+    public fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.criarFormLogin();
+  }
+
+  criarFormLogin(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
 
 }

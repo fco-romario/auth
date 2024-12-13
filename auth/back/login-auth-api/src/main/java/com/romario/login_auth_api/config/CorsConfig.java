e5da9b4de92ab474d8ac34a8e4f9a6dad1e65d87
fco-romario <fco.romario.alves@gmail.com>
2024-12-12 21:52:50 -0300

    Adicionando liberado CORS

---
+++ auth/back/login-auth-api/src/main/java/com/romario/login_auth_api/config/CorsConfig.java
@@ -0,0 +1,14 @@
+package com.romario.login_auth_api.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:4200")
+            .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}

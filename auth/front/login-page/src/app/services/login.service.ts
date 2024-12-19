a44c4fa95d2ce1fb3271d3cbcbce8e4d2ebd7afe
fco-romario <fco.romario.alves@gmail.com>
2024-12-19 20:49:09 -0300

    corrigindo metodo de login em loginService

--- auth/front/login-page/src/app/services/login.service.ts
+++ auth/front/login-page/src/app/services/login.service.ts
@@ -1,17 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginResponse } from '../types/login-response.type';
-import { tap } from 'rxjs';
+import { Observable, Subscriber, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private readonly url = 'http://localhost:8080/auth';
 
   constructor(private http: HttpClient) { }
 
-  login(name: string, password: string) {
-    return this.http.post<LoginResponse>('/login', { name, password }).pipe(
+  login(email: string, password: string) {
+    return this.http.post<LoginResponse>(`${this.url}/login`, { email, password }).pipe(
       tap((value) => {
         sessionStorage.setItem('auth-token', value.token);
         sessionStorage.setItem('auth-name', value.name);
